e0297e58ea84262e2f79ba12a20335664a8116fd
Burunga <monte_marcos@hotmail.com>
2023-12-06 15:59:14 -0300

    Estilizando - Light Mode

--- src/LandingPage/index.js
+++ src/LandingPage/index.js
@@ -6,10 +6,10 @@ export default function LandingPage() {
     return (
         <div className="body">
             <header>
-                <div className="largura-pagina">
-                    <img src="assets\barbearia-logo.png" alt="Logotipo da BarberShop" />
+                <div className="largura-pagina topo">
+                    <img className="logo" src="assets\barbearia-logo.png" alt="Logotipo da BarberShop" />
                     <button>
-                        <img src="assets\moon.png" alt="Simbolo de Sol / Lua para auxiliar a mudança entre Light e Darg" />
+                        <img className="imagem-botao" src="assets\moon.png" alt="Simbolo de Sol / Lua para auxiliar a mudança entre Light e Darg" />
                         <span>Dark</span>
                     </button>
                 </div>
@@ -19,7 +19,7 @@ export default function LandingPage() {
                 <section className="largura-pagina conteudo">
                     <h1>Bem-vindo a Barber Shop</h1>
 
-                    <p className="paragrafo=destaque">Nossa barbearia sempre oferece profissionais de qualidade e estamos prontos para lidar com suas maiores expectativas.</p>
+                    <p className="paragrafo-destaque">Nossa barbearia sempre oferece profissionais de qualidade e estamos prontos para lidar com suas maiores expectativas.</p>
 
                     <p>Nossos serviços são dedicados ao seu sucesso pessoal. Aqui temos uma equipe premiada que demonstrou o talento de mestres barbeiros em vários concursos de estilo. Deixe nosso barbeiro ser seu estilista pessoal e você nunca ficará desapontado.</p>
 
